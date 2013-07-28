cdf353cad7edc30db0f7b1471b8e5f3c30457675
Eric Will <rakaur@malkier.net>
2013-07-28 18:22:33 -0500

    Add an app launcher. Really, it's the equivilent of:
        $ bundle exec ruby -r "./lib/kythera" -e "Kythera.new"
    
    But, it had to be done. Closes #3.

--- lib/kythera.rb
+++ lib/kythera.rb
@@ -2,18 +2,23 @@ require_relative 'kythera/configuration'
 
 # Contains all of the application-wide stuff
 class Kythera
-    # For backwards-incompatible changes
-    V_MAJOR = 0
+  # For backwards-incompatible changes
+  V_MAJOR = 0
 
-    # For backwards-compatible changes
-    V_MINOR = 0
+  # For backwards-compatible changes
+  V_MINOR = 0
 
-    # For minor changes and bugfixes
-    V_PATCH = 1
+  # For minor changes and bugfixes
+  V_PATCH = 1
 
-    # A String representation of the version number
-    VERSION = "#{V_MAJOR}.#{V_MINOR}.#{V_PATCH}"
+  # A String representation of the version number
+  VERSION = "#{V_MAJOR}.#{V_MINOR}.#{V_PATCH}"
 
-    # Our name for things we print out
-    ME = "kythera"
+  # Our name for things we print out
+  ME = "kythera"
+
+  # Get us up and running!
+  def initialize
+    puts "#{ME}: version #{VERSION} [#{RUBY_PLATFORM}]"
+  end
 end
